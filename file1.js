003aee1b25ee6731b1e89814bdb91df380850493
meetarora31 <meeta0260@gmail.com>
2024-08-12 11:39:23 +0530

    operations

---
+++ file1.js
@@ -0,0 +1,102 @@
+// //Save file with name file1.js
+// document.write("CALC","<br>");
+
+
+
+// var x = 12;
+// var y = 13;
+
+// document.write("Addition=",x+y,"<br>")
+// document.write("Subtraction=",x-y,"<br>")
+// document.write("Multiplication=",x*y,"<br>")
+// document.write("Division=",x/y,"<br>")
+
+// -------------------------------
+
+// console.log("Hello, this is console");
+
+// -------------------------------
+
+// var x = 120;
+// function xyz{
+//     var y = 30;
+// }
+// console.log(x,y);
+
+// ---------------
+
+// let x = 12;
+// {
+//     let y = 30
+// }
+// console.log(x,y);
+
+// ------------------
+
+// const x = 12;
+// {
+//     const y = 30
+//     y = 45
+// }
+// console.log(x,y);
+
+// --------------------------------------------------------
+
+// let a = 10;  
+// document.write(a,"<br>")
+// let b = 5;
+// document.write(b,"<br>")
+// let sum = a+b; 
+// document.write(sum,"<br>")
+// let difference = a-b; 
+// document.write(difference,"<br>")
+// let product = a*b;
+// document.write(product,"<br>")
+// let Quotient= a/b;
+// document.write(Quotient,"<br>")
+// let reminder = a%b ;
+// document.write(reminder,"<br>")
+// let isequal = (a==b) ;
+// document.write(isequal,"<br>")
+// let isnotequal = (a!=b) ;
+// document.write(isnotequal,"<br>")
+// let IsGreater = (a>b) ;
+// document.write(IsGreater,"<br>")
+// let LogicalAnd  = (a>b && b<10) ;
+// document.write(LogicalAnd,"<br>")
+
+
+// ------------------------------------------------------
+
+
+// let fname = "John";
+// document.write("Hello ",fname,"<br>")
+// let age = 30;
+// document.write('Age - ',age,"<br>")
+// let isStudent = true;
+// document.write(`Student -${isStudent? "Yes":"No"},<br>`)
+// let emptyValue = "I'm Meet Arora";
+// document.write(`About us - ${emptyValue ||"write Something"} <br>`)
+// let uninitialized;
+// document.write(!uninitialized && "Fill detail completely")
+
+
+// ---------------------------------------------------------
+
+
+// let age = 18;
+// if(age>17){
+//     document.write("Eligible for vote")
+// } else{
+//     document.write("NOt Eligible for vote")
+// }
+// document.write("<br>")
+
+
+// // or
+
+
+// document.write(age> 17 ? "Eligible for vote": "NOt Eligible for vote")
+
+
+// -----------------------------------------------------------
